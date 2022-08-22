967e7ff362726e7e72437c2409e210558465ac89
Samara Letícia <108835942+samletcia@users.noreply.github.com>
2022-08-22 00:16:46 -0300

    Add files via upload

---
+++ tab.js
@@ -0,0 +1,18 @@
+document.getElementById('tabPadrao').click();
+
+function abrirTab(event, idTab) {
+    var conteudos = document.getElementsByClassName('conteudo');
+
+    for (var i = 0; i < conteudos.length; i++) {
+        conteudos[i].style.display = 'none';
+    }
+
+    var tabs = document.getElementsByClassName('tab-button');
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].className = tabs[i].className.replace('ativo', '');
+    }
+    
+
+    document.getElementById(idTab).style.display = 'block';
+    event.currentTarget.className += ' ativo';
+}
